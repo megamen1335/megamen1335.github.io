804be5aaceb4ab540942cf436b8b765e53f4b303
megamen1335 <nikitakos081206@gmail.com>
2026-04-01 09:22:13 +0300

    ПР7

--- js/main.js
+++ js/main.js
@@ -2,71 +2,66 @@ const app = Vue.createApp({
     data() {
         return {
             products: [
-                {
-                    id: 1,
-                    title: "Yellow Onion",
-                    short_text: "Classic cooking onion",
-                    image: "images/onion1.jpg",
-                    desc: "Yellow onion is the most common onion used in cooking."
-                },
-                {
-                    id: 2,
-                    title: "Red Onion",
-                    short_text: "Sweet and colorful",
-                    image: "images/onion2.jpg",
-                    desc: "Red onion is perfect for salads and fresh dishes."
-                },
-                {
-                    id: 3,
-                    title: "White Onion",
-                    short_text: "Sharp flavor",
-                    image: "images/onion3.jpg",
-                    desc: "White onion has a stronger taste and is often used in Mexican cuisine."
-                },
-                {
-                    id: 4,
-                    title: "Green Onion",
-                    short_text: "Fresh and light",
-                    image: "images/onion4.jpg",
-                    desc: "Green onion is used fresh in salads and soups."
-                },
-                {
-                    id: 5,
-                    title: "Sweet Onion",
-                    short_text: "Mild and sweet",
-                    image: "images/onion5.jpg",
-                    desc: "Sweet onion is perfect for caramelizing."
-                }
+                { id: 1, title: "Yellow Onion", short_text: "Classic cooking onion", image: "images/onion1.jpg" },
+                { id: 2, title: "Red Onion", short_text: "Sweet and colorful", image: "images/onion2.jpg" },
+                { id: 3, title: "White Onion", short_text: "Sharp flavor", image: "images/onion3.jpg" },
+                { id: 4, title: "Green Onion", short_text: "Fresh and light", image: "images/onion4.jpg" },
+                { id: 5, title: "Sweet Onion", short_text: "Mild and sweet", image: "images/onion5.jpg" }
             ],
-
             product: {},
-            btnVisible: 0
+            btnVisible: 0,
+            
+            cart: [],
+            contactFields: {},
+            orderPlaced: false
         }
     },
 
     methods: {
         getProduct() {
             const id = window.location.hash.replace('#', '');
-
-            this.product = this.products.find(p => p.id == id);
+            if (id) {
+                this.product = this.products.find(p => p.id == id);
+            }
         },
 
         addToCart(id) {
-            let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-            if (!cart.includes(id)) {
-                cart.push(id);
-                localStorage.setItem('cart', JSON.stringify(cart));
+            let cartIds = JSON.parse(localStorage.getItem('cart')) || [];
+            if (!cartIds.includes(id)) {
+                cartIds.push(id);
+                localStorage.setItem('cart', JSON.stringify(cartIds));
             }
-
             this.btnVisible = 1;
+            this.getCart(); 
+        },
+
+        getCart() {
+            let cartIds = JSON.parse(localStorage.getItem('cart')) || [];
+            this.cart = this.products.filter(product => cartIds.includes(product.id));
+        },
+
+        removeFromCart(id) {
+            
+            this.cart = this.cart.filter(item => item.id !== id);
+            
+            let cartIds = JSON.parse(localStorage.getItem('cart')) || [];
+            cartIds = cartIds.filter(cartId => cartId !== id);
+            localStorage.setItem('cart', JSON.stringify(cartIds));
+        },
+
+        makeOrder() {
+            this.orderPlaced = true;
+            
+            this.cart = [];
+            localStorage.removeItem('cart');
+            
+            console.log("Order Data:", this.contactFields);
         },
 
         checkInCart() {
             const id = window.location.hash.replace('#', '');
-            let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-            if (cart.includes(Number(id))) {
+            let cartIds = JSON.parse(localStorage.getItem('cart')) || [];
+            if (cartIds.includes(Number(id))) {
                 this.btnVisible = 1;
             }
         }
@@ -75,6 +70,7 @@ const app = Vue.createApp({
     mounted() {
         this.getProduct();
         this.checkInCart();
+        this.getCart();
     }
 });
 
